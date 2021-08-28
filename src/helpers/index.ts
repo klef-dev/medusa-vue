f9212e885c0ede6daef971f348d4149532d017be
klefcodes <abrahamugbeshe@gmail.com>
2021-08-28 22:01:36 +0100

    perf: Added return type to formatPrice

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,3 +1,3 @@
-export const formatPrice = (price: number, currency: string) => {
+export const formatPrice = (price: number, currency: string): string => {
   return `${(price / 100).toFixed(2)} ${currency.toUpperCase()}`;
 };
